6b63159990caaea106c6b6ccddea3a73eaa05f76
ValRat <leri.ratu@gmail.com>
2016-02-13 14:02:49 -0800

    Drivers basically done

--- Driver/Driver.ino
+++ Driver/Driver.ino
@@ -1,9 +1,20 @@
-//Arduino PWM Speed Control：
+/**
+ * Movement driver for the robot using PWM as speed variable
+ */
+
+//PIN Definitions
 int E1 = 5;  
 int M1 = 4; 
 int E2 = 6;                      
 int M2 = 7;                        
- 
+
+//Keywords
+boolean LEFT = false;
+boolean RIGHT = true;
+
+//Constants
+int turnTime = 400;
+
 void setup() 
 { 
     pinMode(M1, OUTPUT);   
@@ -12,13 +23,30 @@ void setup()
  
 void loop() 
 { 
-  int value;
-  for(value = 0 ; value <= 255; value+=5) 
-  { 
-    digitalWrite(M1,HIGH);   
-    digitalWrite(M2, HIGH);       
-    analogWrite(E1, value);   //PWM Speed Control
-    analogWrite(E2, value);   //PWM Speed Control
-    delay(30); 
-  }  
+  rotate(LEFT);
+  delay(1000);
+  rotate(RIGHT);
+  delay(1000);
 }
+
+void moveInDirection(int dir, int robotSpeed){
+  digitalWrite(M1, HIGH);
+  digitalWrite(M2, HIGH);
+  analogWrite(E1, robotSpeed);
+  analogWrite(E2, robotSpeed);
+}
+
+void halt(){
+  analogWrite(E1, 0);
+  analogWrite(E2, 0);
+}
+
+void rotate(boolean dir){
+  digitalWrite(M1, dir);
+  digitalWrite(M2, !dir);
+  analogWrite(E1, 255);
+  analogWrite(E2, 255);
+  delay(turnTime);
+  halt();
+}
+
