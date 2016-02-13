a68feb446b7957867890656e1446e1975be29e28
ValRat <leri.ratu@gmail.com>
2016-02-13 15:27:04 -0800

    Cleanup code

--- Driver/Driver.ino
+++ Driver/Driver.ino
@@ -9,8 +9,11 @@ int E2 = 6;
 int M2 = 7;                        
 
 //Keywords
-boolean LEFT = false;
 boolean RIGHT = true;
+boolean LEFT = false;
+boolean FORWARD = true;
+boolean BACK = false;
+int MAX_SPEED = 255;
 
 //Constants
 int turnTime = 400;
@@ -29,23 +32,37 @@ void loop()
   delay(1000);
 }
 
-void moveInDirection(int dir, int robotSpeed){
-  digitalWrite(M1, HIGH);
-  digitalWrite(M2, HIGH);
+/**
+ * Moves the robot in a specified direction at a specified speed
+ * @param dir - the direction the robot is heading
+ *                true for forward, false for backwards
+ * @param robotSpeed - the speed the robot is moving
+ */
+void moveInDirection(boolean dir, int robotSpeed){
+  digitalWrite(M1, dir);
+  digitalWrite(M2, dir);
   analogWrite(E1, robotSpeed);
   analogWrite(E2, robotSpeed);
 }
 
+/**
+ * Stops motors of the robot
+ */
 void halt(){
   analogWrite(E1, 0);
   analogWrite(E2, 0);
 }
 
+/**
+ * Rotates the robot in place
+ * @param dir - the direction the robot is turning
+ *                true for right, false for left
+ */
 void rotate(boolean dir){
   digitalWrite(M1, dir);
   digitalWrite(M2, !dir);
-  analogWrite(E1, 255);
-  analogWrite(E2, 255);
+  analogWrite(E1, MAX_SPEED);
+  analogWrite(E2, MAX_SPEED);
   delay(turnTime);
   halt();
 }
