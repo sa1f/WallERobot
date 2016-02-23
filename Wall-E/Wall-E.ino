a04c4554fcbe037ec58aab19ccb7bd284a174a93
SimonJinaphant <SimonJinaphant@users.noreply.github.com>
2016-02-23 08:56:47 -0800

    Hall effect done, need some refactoring

--- Wall-E/Wall-E.ino
+++ Wall-E/Wall-E.ino
@@ -1,59 +1,22 @@
-/*
-  Arduino Hall Effect Sensor Project
-  by Arvind Sanjeev
-  Please check out  http://diyhacking.com for the tutorial of this project.
-  DIY Hacking
-*/
+const int ULTRASONIC_ECHO_PIN = 11;
+const int ULTRASONIC_TRIG_PIN = 12;
+const int TEMP_SENSOR_PIN = A2;
 
-
-volatile byte half_revolutions;
-unsigned int rpm;
-unsigned long timeold;
-void magnet_detect() {
-  half_revolutions++;
-  Serial.println("detect");
-}
 void setup() {
   Serial.begin(9600);
-  attachInterrupt(0, magnet_detect, RISING);//Initialize the intterrupt pin (Arduino digital pin 2)
-  half_revolutions = 0;
-  rpm = 0;
-  timeold = 0;
 }
 
-void loop(){
-  if (half_revolutions >= 20) {
-    rpm = 30 * 1000 / (millis() - timeold) * half_revolutions;
-    timeold = millis();
-    half_revolutions = 0;
-    Serial.println(rpm,DEC);
-  }
-}
+void loop() {
 
-/*const int ULTRASONIC_ECHO_PIN = 11;
-  const int ULTRASONIC_TRIG_PIN = 12;
-  const int TEMP_SENSOR_PIN = A2;
-
-  const int HALE_PIN = 5;
-  const int REFLECT_OPTIC_PIN = A0;
-
-  void setup() {
-  Serial.begin(9600);
-  pinMode(HALE_PIN, INPUT);
-  }
-
-  void loop() {
-  float hal = digitalRead(HALE_PIN);
-  Serial.println(hal);
   delay(250);
-  }
+}
 
-  void ultrasonic_setup(){
+void ultrasonic_setup() {
   pinMode(ULTRASONIC_ECHO_PIN, INPUT);
   pinMode(ULTRASONIC_TRIG_PIN, OUTPUT);
-  }
+}
 
-  unsigned long ultrasonic_distance() {
+unsigned long ultrasonic_distance() {
   delay(50);
 
   //Read the temperature and calculate the echo denominator
@@ -71,5 +34,5 @@ void loop(){
   unsigned long distance = duration / denom;
 
   return distance;
-  }
-*/
+}
+
